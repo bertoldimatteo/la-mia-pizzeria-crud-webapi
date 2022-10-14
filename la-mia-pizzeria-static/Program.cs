792ba8cf0fe8d1a3f9fb9d239d9e4ab42a38af4d
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-10-14 16:25:15 +0200

    implementanto axios con 404

--- la-mia-pizzeria-static/Program.cs
+++ la-mia-pizzeria-static/Program.cs
@@ -34,7 +34,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Pizza}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.MapRazorPages();
 
