38b9158080b253d3c6df519fd09750215301edb1
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-10-18 16:01:59 +0200

    aggiunto classe Message

---
+++ la-mia-pizzeria-static/Controllers/Api/MessageController.cs
@@ -0,0 +1,12 @@
+﻿using la_mia_pizzeria_crud_mvc.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_crud_mvc.Controllers.Api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+    }
+}
---
+++ la-mia-pizzeria-static/Models/Message.cs
@@ -0,0 +1,24 @@
+﻿namespace la_mia_pizzeria_crud_mvc.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Message()
+        {
+
+        }
+        public Message(int id, string title, string content, string name, string email)
+        {
+            Id = id;
+            Title = title;
+            Content = content;
+            Name = name;
+            Email = email;
+        }
+    }
+}
