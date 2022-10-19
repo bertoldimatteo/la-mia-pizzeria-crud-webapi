62444070872252804cee31e68df3be2daaf69b9b
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-10-19 15:57:16 +0200

    creato model PizzaRepository

---
+++ la-mia-pizzeria-static/Models/PizzaDb.cs
@@ -0,0 +1,6 @@
+﻿namespace la_mia_pizzeria_crud_mvc.Models
+{
+    public class PizzaDb
+    {
+    }
+}
---
+++ la-mia-pizzeria-static/Models/PizzaRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace la_mia_pizzeria_crud_mvc.Models
+{
+    public interface PizzaRepository
+    {
+        public List<Pizza> GetList();
+        public List<Pizza> GetListByFilter(string search);
+        public Pizza GetById(int id);
+        public void Create(Pizza pizza);
+        public void Update(Pizza pizza);
+        public void Delete(Pizza pizza);
+    }
+}
